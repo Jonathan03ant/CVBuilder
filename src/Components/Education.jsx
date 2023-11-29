17a33fc424c6778140f7ca2fcfafc9b3dce5e51e
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-11-29 16:47:54 -0500

    Education Component finished, Not tested

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -20,18 +20,73 @@ export default function Education({
                 <option value="Close"> CLOSE</option>
             </select>
 
-            {!hidden && (
-                <dib className="EducationEditor">
+            {!hidden && educationInfo.map((school) =>(
+                <div className="informationEditor" key={school.id}>
                     <label>
-
+                        <span> School Name</span>
+                        <input  
+                            type="Text"
+                            value={school.school}
+                            onChange={(e) => onEditEducationInfo(school.id, 'school', e.target.value)}
+                        />
                     </label>
 
                     <label>
-
+                        <span> Degree </span>
+                        <input 
+                            type="text"
+                            value={school.degree}
+                            onChange={(e) => onEditEducationInfo(school.id, 'degree', e.target.value)}
+                        />
                     </label>
-                </dib>
 
-            )};
+                    <label>
+                        <span> Location </span>
+                        <input
+                            type="text"
+                            value={school.location}
+                            onChange={(e) => onEditEducationInfo (school.id, 'location', e.target.value)}
+                        />
+                    </label>
+
+                    <label className="half-width">
+                        <span> Start </span>
+                        <input
+                            type="month"
+                            value={school.month}
+                            onChange={(e) => onEditEducationInfo (school.id, 'month', e.target.value)}
+                        />
+                    </label>
+
+                    <label className="half-width">
+                        <span> End </span>
+                        <input
+                            type="month"
+                            value={school.end}
+                            onChange={(e) => onEditEducationInfo (school.id, 'end', e.target.value)}
+                        />
+                    </label>
+
+                {/* We need a button to Delete this School (with school ID) */}
+                    <button
+                        type="button"
+                        className="infoDeletingButton editButton"
+                        onClick={() => onEditEducationInfo(school.id)}
+                    >
+                    Remove School
+                    </button>
+                </div>
+            ))};
+            {/* We need a button to add more schools */}
+            {!hidden &&(
+                <button
+                type="button"
+                className="infomationAddingButton editButton"
+                onClick={onAddEducationInfo}
+                >
+                Add School
+                </button>
+            )}
         </div>
     );
 }
