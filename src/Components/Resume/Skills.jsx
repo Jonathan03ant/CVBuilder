d1e87bd5ca3f42030eca5839750fbda02edfb3e1
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-21 18:27:00 -0500

    Skills Component, DONE

---
+++ src/Components/Resume/Skills.jsx
@@ -0,0 +1,39 @@
+export default function Skills ({
+    details
+}) {
+    if (details.length > 0) {
+        return (
+            <div  className="section-skills">
+                <h2
+                    className="header-skills"
+                    style={{
+                        borderBottom: `1px solid #ddd`,
+                    }}
+                >
+                    Technical Skills
+                </h2>
+                <div className="body-skills">
+                    {details.map((skill) =>
+                        <Skill key={skill.id}
+                        details={skill}
+                        />
+                        )}
+                </div>
+            </div>
+        );
+    }
+}
+
+function Skill ({details}){
+    return (
+        <div className="skills-details">
+            <b className="skill-title">
+                {details.title}
+                :
+            </b>
+            <p className="skill-stack" style={{ fontWeight: 'normal' }}>
+                {details.skills.join(', ')}
+            </p>
+        </div>
+    )
+}
