92ea0cfeec2af100b7266999351d74a539a8fea4
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-21 16:10:52 -0500

    Projects comp Rendering, DONE

---
+++ src/Components/Resume/Projects.jsx
@@ -0,0 +1,53 @@
+import StartToEnd from "./Start-End";
+export default function Projects({
+    details,
+}) {
+    if (details.length > 0) {
+        return(
+            <div className="section-projects">
+                <h2
+                    className="header-projects"
+                    style={{
+                        borderBottom: `1px solid #ddd`,
+                    }}
+                >
+                    Projects
+                </h2>
+                <div className="body-projects">
+                    {details.map((project) =>
+                        <Project key={project.id}
+                        details={project}
+                        />
+                        )}
+                </div>
+            </div>
+        );
+    }
+}
+
+function Project ({details}){
+    return(
+        <div className="projects-details">
+            <div className="project-and-techStack">
+                <h3 className="projects-title">
+                    {details.title}
+                    {" "}
+                    |
+                    {" "}
+                    <span className="project-techStack" style={{ fontWeight: 'normal' }}> {details.techStack}</span>
+                </h3>
+
+                <ul
+                    className="projects-sars">
+                    {details.sars.map((sar) => (
+                        <li className="projects-sar" key={sar.id}>
+                            <div className="bullet-point" />
+                            {sar.sarText}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <StartToEnd start={details.start} end={details.end} />
+        </div>
+    )
+}
