94c2dd907c0d175790954cc6d316643be7cfae66
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-20 23:17:18 -0500

    Date component

---
+++ src/Components/Resume/Start-End.jsx
@@ -0,0 +1,36 @@
+export default function StartToEnd({
+    start, 
+    end
+}) 
+{
+    let startDate;
+    let endDate;
+
+    if (start){
+        startDate = new Date(start);
+        startDate.setDate(startDate.getDate() + 1);
+    }
+
+    if (end) {
+        endDate = new Date(end);
+        endDate.setDate(endDate.getDate() + 1);
+    }
+
+    return (
+        <span className="start-end">
+            {start && (
+                <span>
+                    {startDate.toLocaleString('default', { month: 'short', year: 'numeric' })}
+                    {' - '}
+                </span>
+            )}
+            {end ? (
+                <span> 
+                    {endDate.toLocaleString('default', { month: 'short', year: 'numeric' })}
+                </span>
+            ) : (
+                <span> Present </span>
+            )}
+            </span>
+        );
+    }
